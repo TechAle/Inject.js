17db88bc1cfc29832bc06bff926e5d6f3bc5d44d
TechAle <alessandro.condello.email@gmail.com>
2022-06-08 11:50:38 +0200

    Add: started global injection

--- inject.js
+++ inject.js
@@ -1,6 +1,32 @@
 everyModifications = []
 links = []
 
+function injectGlobal(html, toInject) {
+
+    b = 0
+
+    let globalInject = popNone(toInject)
+    everyModifications = globalInject[0]
+
+    globalInject = globalInject[1]
+
+    let scripts = getScripts(html)
+
+    return html
+}
+
+function popNone(toInject) {
+    let outputInject = []
+    let outputGlobal = []
+    toInject.forEach(e => {
+        if (e.function === "none")
+            outputGlobal.push(e)
+        else
+            outputInject.push(e)
+    })
+    return [outputInject, outputGlobal]
+}
+
 function startIndexing() {
     calculateLinks()
     giveNames()
@@ -9,19 +35,21 @@ function startIndexing() {
 function calculateLinks() {
     links = []
     everyModifications.forEach(e => {
-        let found = false;
-        let index = -1;
-        for(let i = 0; i < links.length; i++) {
-            if (e.file === links[i].file && e.method === links[i].method) {
-                found = true;
-                index = i;
-                break;
+        if (e.function !== "none") {
+            let found = false;
+            let index = -1;
+            for (let i = 0; i < links.length; i++) {
+                if (e.file === links[i].file && e.method === links[i].method) {
+                    found = true;
+                    index = i;
+                    break;
+                }
             }
-        }
-        if (!found) {
-            links.push({file: e.file, method: e.method, toInject: [e]})
-        } else {
-            links[index].toInject.push(e);
+            if (!found) {
+                links.push({file: e.file, method: e.method, toInject: [e]})
+            } else {
+                links[index].toInject.push(e);
+            }
         }
     })
 }
--- injectClassFile.js
+++ injectClassFile.js
@@ -9,6 +9,11 @@ class injectClass {
             throw 'No function defined'
         if (params.pre === undefined && params.at === undefined && params.post === undefined)
             throw 'No body defined'
+        if (params.function === "none") {
+            if (params.at !== undefined && (params.pre !== undefined || params.post !== undefined)
+                || (params.pre !== undefined && params.post !== undefined))
+                throw "You can only have 1 inject for none functions"
+        }
         if (params.at !== undefined && params.line === undefined)
             throw 'No line defined for at'
         this.file = params.file;
